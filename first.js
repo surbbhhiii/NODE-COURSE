5cb95fdcf2ed138d10edbf2b70dbc8be69156ebd
Student ApnaCollege <Student@apnacollege.in>
2026-03-15 22:15:27 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("kg Coding is the best.");
+
+const fs = require('fs');
+fs.writeFile('output.txt', "Writing File ", (err) => {
+
+
+  if (err) console.log("Error occurred");
+  else console.log("File Written Successfully");
+});
+
+
+
+
